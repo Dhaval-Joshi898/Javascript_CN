c02d5e7b6c1da09117b3e4a796407e569bd9cd39
Dhaval <joshidhaval2002@gmail.com>
2023-08-26 16:23:57 +0530

    facts about datatypes (Infinity) and "-0" in js

--- datatype.js
+++ datatype.js
@@ -30,3 +30,22 @@ console.log(typeof news)        //type is "object"   this is a bug in JS
 //BigInt(ES2020)
 let num1=10n;           //ends with n   //number greate than 64 bit to stored used bigint
 console.log(typeof num1)   //type is bigint
+
+
+//Interesting facts about datatypes
+
+const numbers1=2/0;
+console.log(numbers1)   //Infinity
+
+const number2=Infinity/2;
+console.log(number2)   //Infinity
+
+const number3=Infinity/Infinity;
+console.log(number3)   //NaN
+
+
+const number4=-8/Infinity;
+console.log(number4)   //-0 <--***********
+
+
+console.log(-0+0)   //op 0
