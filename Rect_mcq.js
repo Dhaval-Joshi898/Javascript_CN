952e25fcf663cae6704d5473a011ed1fd7b023c7
Dhaval <joshidhaval2002@gmail.com>
2023-08-26 20:56:27 +0530

    rectangle area JS program

---
+++ Rect_mcq.js
@@ -0,0 +1,22 @@
+
+// let e="20";
+// let f="5";
+// let result=e<f;
+// console.log(result)
+// let a=( "hello" != "Hello")
+// console.log(a)
+
+function main(len,wid){
+    let result;
+    //Write the program to calculate the area of rectangle using len and wid as length and width of rectangle
+    
+    result=len*wid;
+    
+    
+    return result;
+    }
+
+let length=5;
+let width=5;
+let area=main(length,width);
+console.log(area);
