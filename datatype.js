3884ff091a089eb139c0160f73be5e87c112e1e4
Dhaval <joshidhaval2002@gmail.com>
2023-08-26 15:40:42 +0530

    DataTypes with example

--- datatype.js
+++ datatype.js
@@ -1,2 +1,30 @@
+// Two types od datatypes
+// 1.Object   2.Primitive DataTypes
+
+
+/* ***********************************************************
+************** Primitive DataTypes*********************/
+
 let num=10;
-console.log(typeof(num))
+console.log(typeof(num))    //type number
+
+const firstName="DHAVAL";
+console.log(typeof firstName)    //type string
+
+let bool=true;
+console.log(typeof bool)    //type boolean
+
+
+let a;
+console.log(typeof a)     //type undefined
+
+
+let news=null;                  //here i have given value null 
+console.log(typeof news)        //type is "object"   this is a bug in JS
+
+
+//More dataypes are
+
+// Symbol(ES6/2015)
+
+//BigInt(ES2020)
