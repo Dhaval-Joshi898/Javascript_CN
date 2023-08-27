37fabf80235cd692e96ea7d447b6e39e68b15c9f
Dhaval <joshidhaval2002@gmail.com>
2023-08-27 18:06:09 +0530

    type conversion examples

---
+++ typeConversion.js
@@ -0,0 +1,42 @@
+/** Type Conversion */
+
+/*  *** Convert to String ***  */
+// String();
+
+console.log(String(100));
+console.log(String(true));
+console.log(String(null));
+console.log(String(undefined));
+
+// toString();
+
+console.log((123).toString());
+// console.log(null.toString());
+
+/** Convert to Number */
+// Number();
+
+console.log('***NUMBER***');
+
+console.log(Number('25'));
+console.log(Number('Value'));
+console.log(Number(false));
+
+// Unary + Operator --- works on Variables
+
+let a = '123falak';
+console.log(+a);    //give NaN error NOt a Number
+
+// parseFloat();
+console.log(parseFloat('123.45f'));
+
+// parseInt();
+console.log(parseInt('123.45f'));
+
+/** Convert to Boolean */
+// Boolean();
+
+console.log(Boolean(undefined));
+
+// boolean conversion can also be done by using !! operator
+console.log(!!(undefined));
