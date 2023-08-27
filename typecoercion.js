a2da4597f7a20f5a20afe09ff2791ba66f9c368a
Dhaval <joshidhaval2002@gmail.com>
2023-08-27 16:03:09 +0530

    Added more examples

--- typecoercion.js
+++ typecoercion.js
@@ -5,3 +5,15 @@ let a='5'+5
 console.log(a);
 console.log(typeof a);
 
+let sub='55'-10;
+console.log(sub)
+
+let mul='10'*10;
+console.log(mul)
+
+let div='10'/5;
+console.log(div)
+
+
+let pow='2'**4;
+console.log(pow)
